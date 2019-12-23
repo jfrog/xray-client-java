7413fa717c35c5fbd6bdf9b9b118b0bc224147c2
yahavi <yahavi@jfrog.com>
2019-12-23 19:55:43 +0200

    Add support for Keystore provider to allow self signed certificates

--- src/main/java/com/jfrog/xray/client/impl/XrayClient.java
+++ src/main/java/com/jfrog/xray/client/impl/XrayClient.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.jfrog.client.http.HttpBuilderBase;
 import org.jfrog.client.http.auth.PreemptiveAuthInterceptor;
 import org.jfrog.client.http.model.ProxyConfig;
+import org.jfrog.client.util.KeyStoreProvider;
 
 /**
  * Created by romang on 2/2/17.
@@ -21,7 +22,8 @@ public class XrayClient {
     /**
      * Username, API key, and custom url
      */
-    public static Xray create(String url, String username, String password, String userAgent, boolean trustSelfSignCert, ProxyConfig proxyConfig) {
+    public static Xray create(String url, String username, String password, String userAgent,
+                              boolean noHostVerification, KeyStoreProvider keyStoreProvider, ProxyConfig proxyConfig) {
         HttpBuilderBase configurator = new HttpBuilderBase() {
         };
         configurator.hostFromUrl(url)
@@ -30,13 +32,15 @@ public class XrayClient {
                 .socketTimeout(CONNECTION_TIMEOUT_MILLISECONDS)
                 .userAgent(userAgent)
                 .proxy(proxyConfig)
-                .trustSelfSignCert(trustSelfSignCert)
+                .noHostVerification(noHostVerification)
+                .keyStoreProvider(keyStoreProvider)
+                .trustSelfSignCert(true)
                 .addRequestInterceptor(new PreemptiveAuthInterceptor());
 
         return new XrayImpl(configurator.build(), url);
     }
 
-    public static Xray create(String url, String username, String password, boolean trustSelfSignCert, ProxyConfig proxyConfig) {
-        return create(url, username, password, DEFAULT_USER_AGENT, trustSelfSignCert, proxyConfig);
+    public static Xray create(String url, String username, String password, boolean noHostVerification, KeyStoreProvider keyStoreProvider, ProxyConfig proxyConfig) {
+        return create(url, username, password, DEFAULT_USER_AGENT, noHostVerification, keyStoreProvider, proxyConfig);
     }
 }
