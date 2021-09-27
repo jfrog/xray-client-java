373331c4590411d9f25f2503bc05c5f6408f9183
Tal Arian <tala@jfrog.com>
2021-09-27 10:18:23 +0300

    Add missing ImpactPathImpl class

---
+++ src/main/java/com/jfrog/xray/client/impl/services/graph/ImpactPathImpl.java
@@ -0,0 +1,29 @@
+package com.jfrog.xray.client.impl.services.graph;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jfrog.xray.client.services.graph.ImpactPath;
+
+public class ImpactPathImpl implements ImpactPath {
+
+    private String id;
+    private String fullPath;
+    @Override
+    @JsonProperty("component_id")
+    public String getComponentId() {
+        return id;
+    }
+    @JsonProperty("component_id")
+    public void setComponentId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    @JsonProperty("full_path")
+    public String getFullPath() {
+        return fullPath;
+    }
+    @JsonProperty("full_path")
+    public void setFullPath(String fullPath) {
+        this.fullPath = fullPath;
+    }
+}
