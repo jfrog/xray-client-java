f09d2e109b397622824e2731b9e94817c6a8422c
Tal Arian <tala@jfrog.com>
2021-09-30 16:48:05 +0300

    Add missing ImpactPath interface

---
+++ src/main/java/com/jfrog/xray/client/services/graph/ImpactPath.java
@@ -0,0 +1,10 @@
+package com.jfrog.xray.client.services.graph;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public interface ImpactPath {
+    @JsonProperty("component_id")
+    String getComponentId();
+    @JsonProperty("full_path")
+    String getFullPath();
+}
