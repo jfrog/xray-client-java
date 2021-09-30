59aa02b90736c204e1ced2d0e56946082de43599
Tal Arian <tala@jfrog.com>
2021-09-30 21:45:19 +0300

    Add check cancel to graph scan

--- src/main/java/com/jfrog/xray/client/impl/services/graph/GraphImpl.java
+++ src/main/java/com/jfrog/xray/client/impl/services/graph/GraphImpl.java
@@ -38,45 +38,47 @@ public class GraphImpl implements Graph {
     }
 
     @Override
-    public GraphResponse graph(DependencyTree dependencies, String projectKey) throws IOException, InterruptedException {
+    public GraphResponse graph(DependencyTree dependencies, Runnable checkCanceled, String projectKey) throws IOException, InterruptedException {
         if (projectKey == null || projectKey.isEmpty()) {
-            return graph(dependencies);
+            return graph(dependencies, checkCanceled);
         }
         if (dependencies == null) {
             return new GraphResponseImpl();
         }
-        return this.post("?project=" + projectKey, dependencies);
+        return this.post("?project=" + projectKey, dependencies, checkCanceled);
     }
 
     @Override
-    public GraphResponse graph(DependencyTree dependencies) throws IOException, InterruptedException {
+    public GraphResponse graph(DependencyTree dependencies, Runnable checkCanceled) throws IOException, InterruptedException {
         if (dependencies == null) {
             return new GraphResponseImpl();
         }
-        return this.post("", dependencies);
+        return this.post("", dependencies, checkCanceled);
     }
 
-    private GraphResponse post(String params, Object body) throws IOException, InterruptedException {
+    private GraphResponse post(String params, Object body, Runnable checkCanceled) throws IOException, InterruptedException {
         HttpEntity entity = null;
         // First, request a scan from Xray.
         try (CloseableHttpResponse response = xray.post("scan/graph" + params, body, mapper)) {
+            checkCanceled.run();
             entity = response.getEntity();
             GraphResponse requestResponse = mapper.readValue(response.getEntity().getContent(), GraphResponseImpl.class);
             String scanId = requestResponse.getScanId();
             // If no context was provided (project name), we would like to receive all known vulnerabilities.
             String includeVulnerabilities = !params.isEmpty() ? "" : "&include_vulnerabilities=true";
-            return getGraphScanResults(scanId, includeVulnerabilities);
+            return getGraphScanResults(scanId, includeVulnerabilities, checkCanceled);
 
         } finally {
             EntityUtils.consumeQuietly(entity);
         }
     }
 
-    private GraphResponse getGraphScanResults(String scanId, String includeVulnerabilities) throws IOException, InterruptedException {
+    private GraphResponse getGraphScanResults(String scanId, String includeVulnerabilities, Runnable checkCanceled) throws IOException, InterruptedException {
         HttpEntity entity = null;
         // Xray will respond with 201 until the completion of the scan. Once completed, 200 will be returned.
         for (int i = 0; i < MAX_ATTEMPTS; i++) {
             try (CloseableHttpResponse res = xray.get("scan/graph/" + scanId + "?include_licenses=true" + includeVulnerabilities)) {
+                checkCanceled.run();
                 StatusLine statusLine = res.getStatusLine();
                 int statusCode = statusLine.getStatusCode();
                 if (statusCode == HttpStatus.SC_OK) {
