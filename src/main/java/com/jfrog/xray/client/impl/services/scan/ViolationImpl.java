2e54cd84f2293758b331db543539225b661d5add
Yahav Itzhak <yahavi@users.noreply.github.com>
2022-12-18 17:55:08 +0200

    Add more fields to the scan/graph response (#36)

--- src/main/java/com/jfrog/xray/client/impl/services/scan/ViolationImpl.java
+++ src/main/java/com/jfrog/xray/client/impl/services/scan/ViolationImpl.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jfrog.xray.client.impl.services.common.CveImpl;
 import com.jfrog.xray.client.services.common.Cve;
 import com.jfrog.xray.client.services.scan.Component;
+import com.jfrog.xray.client.services.scan.ExtendedInformation;
 import com.jfrog.xray.client.services.scan.Violation;
 
 import java.util.List;
 import java.util.Map;
 
 public class ViolationImpl implements Violation {
+    private String updated;
     private String summary;
     private String severity;
     private String issueId;
@@ -19,6 +21,14 @@ public class ViolationImpl implements Violation {
     private List<CveImpl> cves;
     private List<String> references;
     private String ignoreRuleUrl;
+    private String watchName;
+    private ExtendedInformationImpl extendedInformation;
+
+    @Override
+    @JsonProperty("updated")
+    public String getUpdated() {
+        return updated;
+    }
 
     @Override
     @JsonProperty("cves")
@@ -72,5 +82,17 @@ public class ViolationImpl implements Violation {
     @JsonProperty("license_name")
     public String getLicenseName() {
         return licenseName;
+    }
+
+    @Override
+    @JsonProperty("watch_name")
+    public String getWatchName() {
+        return watchName;
+    }
+
+    @Override
+    @JsonProperty("extended_information")
+    public ExtendedInformation getExtendedInformation() {
+        return extendedInformation;
     }
 }
