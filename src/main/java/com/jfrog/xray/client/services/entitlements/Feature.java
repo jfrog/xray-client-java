fe4d41ceeeea01976426e08010b23d92efc03673
Tal Arian <tala@jfrog.com>
2023-05-28 18:56:10 +0300

    Add the new Feature definition for entitlements service

--- src/main/java/com/jfrog/xray/client/services/entitlements/Feature.java
+++ src/main/java/com/jfrog/xray/client/services/entitlements/Feature.java
@@ -1,7 +1,10 @@
 package com.jfrog.xray.client.services.entitlements;
 
 public enum Feature {
-    ContextualAnalysis("contextual_analysis");
+    ContextualAnalysis("contextual_analysis"),
+    InfrastructureAsCode("iac_scanners"),
+    SecretsScanner("secrets_detection");
+
     private final String name;
 
     Feature(String name) {
